99c673e46fc2fec346238532fdbc33283b1653b9
Sarah <s6vargas@ucsd.edu>
2016-02-18 19:42:44 +0000

    initial analytics lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,4 +10,15 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	$('.likeBtn').click(projectClick);
+}
+
+function projectClick(e) { 
+	console.log("anything");
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event     
+    ga("send", "event", 'like', 'click'); 
+    //$(this).css("background-color", "#7fff00");
 }
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,20 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
+	projects["grid"] = false;
   	res.render('index', projects);
 };
+
+exports.viewGrid = function(req, res){
+	projects["grid"] = true;
+	res.render('index', projects);
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event     
+    ga("send", "event", 'like', 'click'); 
+    $(this).css("background-color", "#7fff00");
+}
